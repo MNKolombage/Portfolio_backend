5131583bf58d02fceb35fa3106094959e5539d07
Maleesha Kolombage <141763032+MNKolombage@users.noreply.github.com>
2024-06-05 10:54:52 +0530

    Create Blog.js

---
+++ Blog.js
@@ -0,0 +1,11 @@
+const mongoose = require('./db');
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    date: Date
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
